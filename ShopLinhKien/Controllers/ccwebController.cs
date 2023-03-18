174ced4a5ec46c21b0bfa619686ea808aaefcdc4
Khánh Võ An <voankhanh2k2@gmail.com>
2023-03-18 14:16:57 +0700

    asdasasdasasddasasds

---
+++ ShopLinhKien/Controllers/ccwebController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ShopLinhKien.Controllers
+{
+    public class ccwebController : Controller
+    {
+        // GET: ccweb
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
